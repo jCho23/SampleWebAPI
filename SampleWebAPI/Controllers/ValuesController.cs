7e8c595ad826237dd7b59b71a3cbdd7eab51b446
June <junecho.ucb@gmail.com>
2018-02-27 09:34:48 -0800

    Initial commit

---
+++ SampleWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleWebAPI.Models;
+using System.Collections.ObjectModel;
+
+namespace SampleWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Message> Get()
+        {
+            var list = new List<Message>();
+
+            Message firstMessage = new Message();
+
+            firstMessage.OriginalMessage = "Its good!";
+
+            list.Add(firstMessage);
+
+            return list;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int userId)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SampleWebAPI/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SampleWebAPI.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string OriginalMessage { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Message()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+        }
+
+
+    }
+
+
+
+}
